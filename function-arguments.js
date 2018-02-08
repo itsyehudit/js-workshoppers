47195657822f03085b9c80ce1d27a512aade2b37
Katarzyna Mildner <itsyehudit@gmail.com>
2018-02-08 02:12:53 +0100

    Deletion of unnecessary files outside the javascripting folder.

--- function-arguments.js
+++
@@ -1,4 +0,0 @@
-function math(Huey, Dewey, Louie) {
-  return Dewey*Louie+Huey
-};
-console.log(math(53, 61, 67));
--- if-statement.js
+++
@@ -1,7 +0,0 @@
-var fruit = 'orange';
-fruit.length;
-if (fruit.length > 5) {
-  console.log('The fruit name has more than five characters.');
-} else {
-  console.log('The fruit name has five characters or less.');
-}
